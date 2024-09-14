4af79f4749ac43ad08df127d58cd3cc51f31a497
santiparra <122759810+Santiparra@users.noreply.github.com>
2024-09-14 14:38:29 -0300

    Structs module with a couple basic ex

---
+++ Structs/Embedded_structs.go
@@ -0,0 +1,30 @@
+package main
+
+type User struct {
+	Name string
+	Membership
+}
+
+type Membership struct{
+	Type             string
+	MessageCharLimit int
+}
+
+func newUser(name string, membershipType string) User {
+	if membershipType == "premium" {
+		return User{
+			Name: name, 
+			Membership: Membership{ 
+				Type: membershipType, 
+				MessageCharLimit: 1000,
+			},
+		}
+	}
+	return User{
+		Name: name, 
+		Membership: Membership{ 
+			Type: membershipType, 
+			MessageCharLimit: 100,
+		},
+	}
+}
---
+++ Structs/Structs_methods.go
@@ -0,0 +1,29 @@
+package main
+
+func (user User) SendMessage(message string, messageLength int) (string, bool) {
+	if messageLength <= user.MessageCharLimit {
+		return message, true
+	}
+	return "", false
+} 
+
+type User struct {
+	Name string
+	Membership
+}
+
+type Membership struct {
+	Type             string
+	MessageCharLimit int
+}
+
+func newUser(name string, membershipType string) User {
+	membership := Membership{Type: membershipType}
+	if membershipType == "premium" {
+		membership.MessageCharLimit = 1000
+	} else {
+		membership.Type = "standard"
+		membership.MessageCharLimit = 100
+	}
+	return User{Name: name, Membership: membership}
+}
